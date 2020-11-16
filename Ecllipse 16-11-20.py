15ffa88f6d37cf07daf1528d13256d0c63978fc8
SaveOurSoul <41711796+SaveOurSoul@users.noreply.github.com>
2020-11-16 16:21:31 +0700

    Create Ecllipse 16-11-20.py

---
+++ Ecllipse 16-11-20.py
@@ -0,0 +1,46 @@
+def loop():
+	for y in range (0,10):
+		for z in range (0,8):
+			print(" ",end='')
+		else:
+			print("*      *")
+def loop1():
+	for a in range (0,11):
+		print(" ",end='')
+	else:
+		print("***")
+def loop2():		
+	for b in range (0,9):
+		print(" ",end='')
+	else:
+		print("*     *")
+def loop3():		
+	for c in range (0,7):
+		print(" ",end='')
+	else:
+		print("*         *")
+def loop4():		
+	for c in range (0,6):
+		print(" ",end='')
+	else:
+		print("*           *")
+def loop5():		
+	for c in range (0,5):
+		print(" ",end='')
+	else:
+		print("*             *")
+
+loop1()
+loop2()
+loop3()
+loop4()
+loop5()
+loop5()
+loop5()
+loop4()
+loop3()
+loop2()
+loop1()
+
+import turtle
+turtle.forward(15)
